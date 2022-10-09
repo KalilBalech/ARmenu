a764edbaeec9392b67f24af017f05518137a5c31
Kalil Georges Balech <kalilgb152@gmail.com>
2022-10-09 16:20:51 -0300

    changed cors origin and added middleware with Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const bodyParser = require('body-parser')
 const spawn = require('child_process').spawn
 
 const app = express()
-app.use(cors({origin: "*"}))
+app.use(cors({origin: "http://localhost:8080/carrossel-de-cenas/"}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.post('/order', (req, res) => {
+function middleware(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/carrossel-de-cenas/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+}
+
+app.post('/order', middleware, (req, res) => {
   const childPython = spawn('python', ['./pythonWhatsAppMessaging/main.py', 'Bagaceira'])
   childPython.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`)
