5dc94affedc5becc9424befe099173a3b30a7436
Kalil Georges Balech <kalilgb152@gmail.com>
2022-10-09 16:32:43 -0300

    removed middleware and removed carrossel-de-cenas

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const bodyParser = require('body-parser')
 const spawn = require('child_process').spawn
 
 const app = express()
-app.use(cors({origin: "http://localhost:8080/carrossel-de-cenas/"}))
+app.use(cors({origin: "http://localhost:8080"}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-function middleware(req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/carrossel-de-cenas/');
+// function middleware(req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-}
+//   // Pass to next layer of middleware
+//   next();
+// }
 
-app.post('/order', middleware, (req, res) => {
+app.post('/order', (req, res) => {
   const childPython = spawn('python', ['./pythonWhatsAppMessaging/main.py', 'Bagaceira'])
   childPython.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`)
@@ -40,6 +40,7 @@ app.post('/order', middleware, (req, res) => {
   res.send('A rota post deu certo!')
 })
 // essa process.env.PORT é definida pelo render
+// process.env.PORT
 app.listen(process.env.PORT, ()=>{
   console.log("API rodando!")
 })
